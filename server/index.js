3b5ed009ee670813fbb4d06b7e644efa28d65f30
D-a-k-s-h <daksh.kumar.ug22@gmail.com>
2025-04-01 02:24:02 +0530

    Enable production static file serving in server index

--- server/index.js
+++ server/index.js
@@ -28,13 +28,13 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname,'../frontend/dist')));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
-//     app.get("*",(req,res) => {
-//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-//     })
-// }
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 app.use(
     fileUpload({
