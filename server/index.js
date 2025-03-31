c35657711ebafbe12d22ca288bcb94ee8a3f1ade
D-a-k-s-h <daksh.kumar.ug22@gmail.com>
2025-04-01 02:21:33 +0530

    Comment out production static file serving in server index

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ server.listen(PORT, () => {
     console.log(`Server connected to port = ${PORT}`);
 });
 
-const __dirname = path.resolve();
 
 app.use(
     cors({
@@ -29,13 +28,13 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
-    app.get("*",(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
+//     app.get("*",(req,res) => {
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     })
+// }
 
 app.use(
     fileUpload({
